709aec2eafeaf4e77ba5b6a404775f8b06001ac2
Fernando <fernandoaguileradealba@Fernandos-MacBook-Pro.local>
2024-03-27 16:31:53 +0100

    added abr waveform plotting script

--- ABR/abr_setup.m
+++ ABR/abr_setup.m
@@ -7,9 +7,9 @@ close all;
 clear; clc;
 global abr_root_dir abr_data_dir abr_out_dir animal ChinCondition ChinFile ChinID PROJdir
 %% Animal ID
-ChinID = 'Q445';
-ChinCondition = 'post';
-ChinFile = 'D14';
+ChinID = 'Q438';
+ChinCondition = 'pre';
+ChinFile = 'Baseline_2';
 %% Directories
 % PROJdir = directory containing project folder
 % abr_data_dir = directory containing data folder
