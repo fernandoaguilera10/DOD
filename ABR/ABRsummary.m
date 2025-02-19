985e77c1c782bd337e18411b5fecc63d565f0cb0
Fernando Fernando Aguilera De Alba <aguilerl@purdue.edu>
2025-02-19 02:39:57 -0500

    added automated ABR peak picking feature

--- ABR/ABRsummary.m
+++ ABR/ABRsummary.m
@@ -1,11 +1,11 @@
-function ABRsummary(outpath,OUTdir,Conds2Run,Chins2Run,ChinIND,CondIND,idx_plot_relative,ylimits_ind_threshold,ylimits_ind_peaks,ylimits_ind_lat,ylimits_avg_threshold,ylimits_avg_peaks,ylimits_avg_lat,colors,shapes,analysis_type)% ABR summary
+function ABRsummary(outpath,OUTdir,Conds2Run,Chins2Run,ChinIND,CondIND,idx_plot_relative,ylimits_ind_threshold,ylimits_ind_peaks,ylimits_ind_lat,ylimits_avg_threshold,ylimits_avg_peaks,ylimits_avg_lat,colors,shapes,analysis_type1, analysis_type2)% ABR summary
 global abr_f abr_thresholds abr_peaks_amp abr_peaks_lat abr_peaks_f abr_peaks_label abr_peaks_level abr_peaks_waveform abr_peaks_waveform_time
 cwd = pwd;
 %% INDIVIDUAL PLOTS
 condition = strsplit(Conds2Run{CondIND}, filesep);
 if exist(outpath,"dir")
     cd(outpath)
-    if strcmp(analysis_type,'Thresholds') 
+    if strcmp(analysis_type1,'Thresholds')
         search_file = cell2mat([Chins2Run(ChinIND),'_',condition{1},condition{2},'_ABRthresholds.mat']);
         datafile = load_files(outpath,search_file);
         load(datafile);
@@ -13,21 +13,27 @@ if exist(outpath,"dir")
         % PLOTTING SPL
         abr_f{ChinIND,CondIND} = abr_out.freqs;
         abr_thresholds{ChinIND,CondIND} = abr_out.thresholds;
-        plot_ind_abr(abr_out,analysis_type,colors,shapes,Conds2Run,Chins2Run,ChinIND,CondIND,outpath,ylimits_ind_threshold,[],[])
-    elseif strcmp(analysis_type,'Peaks')
-        %% TBD
-        search_file = cell2mat(['*',Chins2Run(ChinIND),'_',condition{1},condition{2},'_ABRpeaks*.mat']);
-        datafile = load_files(outpath,search_file);
-        load(datafile);
-        cd(cwd);
+        plot_ind_abr(abr_out,analysis_type1,colors,shapes,Conds2Run,Chins2Run,ChinIND,CondIND,outpath,ylimits_ind_threshold,[],[])
+    elseif strcmp(analysis_type1,'Peaks')
+        if strcmp(analysis_type2,'Manual')
+            search_file = cell2mat(['*',Chins2Run(ChinIND),'_',condition{1},condition{2},'_ABRpeaks*.mat']);
+            datafile = load_files(outpath,search_file);
+            load(datafile);
+            cd(cwd);
+        elseif strcmp(analysis_type2,'DTW')
+            search_file = cell2mat(['*',Chins2Run(ChinIND),'_',condition{1},condition{2},'_ABRpeaks_dtw*.mat']);
+            datafile = load_files(outpath,search_file);
+            load(datafile);
+            cd(cwd);
+        end
         abr_peaks_amp{ChinIND,CondIND} = abrs.plot.peak_amplitude;
         abr_peaks_lat{ChinIND,CondIND} = abrs.plot.peak_latency;
         abr_peaks_f{ChinIND,CondIND} = abrs.plot.freq;
-        abr_peaks_label{ChinIND,CondIND} = abrs.plot.peaks;
+        %abr_peaks_label{ChinIND,CondIND} = abrs.plot.peaks;
         abr_peaks_level{ChinIND,CondIND} = abrs.plot.levels;
         abr_peaks_waveform{ChinIND,CondIND} = abrs.plot.waveforms;
         abr_peaks_waveform_time{ChinIND,CondIND} = abrs.plot.waveforms_time;
-        plot_ind_abr(abrs.plot,analysis_type,colors,shapes,Conds2Run,Chins2Run,ChinIND,CondIND,outpath,[],ylimits_ind_peaks,ylimits_ind_lat)
+        plot_ind_abr(abrs.plot,analysis_type1,colors,shapes,Conds2Run,Chins2Run,ChinIND,CondIND,outpath,[],ylimits_ind_peaks,ylimits_ind_lat)
     end
     cd(cwd)
 else
@@ -35,27 +41,42 @@ else
 end
 %% AVERAGE PLOTS (individual + average)
 if ChinIND == length(Chins2Run) && CondIND == length(Conds2Run)
-    if strcmp(analysis_type,'Thresholds')
+    if strcmp(analysis_type1,'Thresholds')
         fig_num_avg = length(Chins2Run)+1;
         % Plot individual lines
-        [average,idx] = avg_abr(abr_f,abr_thresholds,Chins2Run,Conds2Run,fig_num_avg,colors,shapes,idx_plot_relative,analysis_type,[]);
+        [average,idx] = avg_abr(abr_f,abr_thresholds,Chins2Run,Conds2Run,fig_num_avg,colors,shapes,idx_plot_relative,analysis_type1,[]);
         % Plot average lines
         outpath = strcat(OUTdir,filesep,'ABR');
         filename = 'ABR_Thresholds_Average_dBSPL';
-        plot_avg_abr(average,analysis_type,colors,shapes,idx,Conds2Run,outpath,filename,fig_num_avg,ylimits_avg_threshold,idx_plot_relative)
-    elseif strcmp(analysis_type,'Peaks')
-        % Peak-to-peak amplitude
-        fig_num_avg = ((ChinIND - 1) * length(Conds2Run) + CondIND)+1;
-        [average,idx] = avg_abr(abr_peaks_level,abr_peaks_amp,Chins2Run,Conds2Run,fig_num_avg,colors,shapes,idx_plot_relative,analysis_type,'Amplitude');
-        outpath = strcat(OUTdir,filesep,'ABR');
-        filename = 'ABR_PeakAmplitude_Average';
-        plot_avg_abr(average,analysis_type,colors,shapes,idx,Conds2Run,outpath,filename,fig_num_avg,[],idx_plot_relative,'Amplitude')
-        % Peak latency
-        fig_num_avg = ((ChinIND - 1) * length(Conds2Run) + CondIND)+7;
-        [average,idx] = avg_abr(abr_peaks_level,abr_peaks_lat,Chins2Run,Conds2Run,fig_num_avg,colors,shapes,idx_plot_relative,analysis_type,'Latency');
-        outpath = strcat(OUTdir,filesep,'ABR'); cd(cwd);
-        filename = 'ABR_PeakLatency_Average';
-        plot_avg_abr(average,analysis_type,colors,shapes,idx,Conds2Run,outpath,filename,fig_num_avg,[],idx_plot_relative,'Latency')
+        plot_avg_abr(average,analysis_type1,colors,shapes,idx,Conds2Run,outpath,filename,fig_num_avg,ylimits_avg_threshold,idx_plot_relative)
+    elseif strcmp(analysis_type1,'Peaks')
+        if strcmp(analysis_type1,'Manual')
+            % Peak-to-peak amplitude
+            fig_num_avg = ((ChinIND - 1) * length(Conds2Run) + CondIND)+1;
+            [average,idx] = avg_abr(abr_peaks_level,abr_peaks_amp,Chins2Run,Conds2Run,fig_num_avg,colors,shapes,idx_plot_relative,analysis_type1,'Amplitude');
+            outpath = strcat(OUTdir,filesep,'ABR');
+            filename = 'ABR_PeakAmplitude_Average';
+            plot_avg_abr(average,analysis_type1,colors,shapes,idx,Conds2Run,outpath,filename,fig_num_avg,[],idx_plot_relative,'Amplitude')
+            % Peak latency
+            fig_num_avg = ((ChinIND - 1) * length(Conds2Run) + CondIND)+7;
+            [average,idx] = avg_abr(abr_peaks_level,abr_peaks_lat,Chins2Run,Conds2Run,fig_num_avg,colors,shapes,idx_plot_relative,analysis_type1,'Latency');
+            outpath = strcat(OUTdir,filesep,'ABR'); cd(cwd);
+            filename = 'ABR_PeakLatency_Average';
+            plot_avg_abr(average,analysis_type1,colors,shapes,idx,Conds2Run,outpath,filename,fig_num_avg,[],idx_plot_relative,'Latency')
+        elseif strcmp(analysis_type2,'DTW')
+            % Peak-to-peak amplitude
+            fig_num_avg = ((ChinIND - 1) * length(Conds2Run) + CondIND)+1;
+            [average,idx] = avg_abr(abr_peaks_level,abr_peaks_amp,Chins2Run,Conds2Run,fig_num_avg,colors,shapes,idx_plot_relative,analysis_type1,'Amplitude');
+            outpath = strcat(OUTdir,filesep,'ABR');
+            filename = 'ABR_PeakAmplitude_Average_dtw';
+            plot_avg_abr(average,analysis_type1,colors,shapes,idx,Conds2Run,outpath,filename,fig_num_avg,[],idx_plot_relative,'Amplitude')
+            % Peak latency
+            fig_num_avg = ((ChinIND - 1) * length(Conds2Run) + CondIND)+2;
+            [average,idx] = avg_abr(abr_peaks_level,abr_peaks_lat,Chins2Run,Conds2Run,fig_num_avg,colors,shapes,idx_plot_relative,analysis_type1,'Latency');
+            outpath = strcat(OUTdir,filesep,'ABR'); cd(cwd);
+            filename = 'ABR_PeakLatency_Average_dtw';
+            plot_avg_abr(average,analysis_type1,colors,shapes,idx,Conds2Run,outpath,filename,fig_num_avg,[],idx_plot_relative,'Latency')
+        end
     end
 end
 cd(cwd);
--- analysis_menu.m
+++ analysis_menu.m
@@ -1,4 +1,4 @@
-function [EXPname, EXPname2] = analysis_menu()
+function [EXPname, EXPname2, EXPname3] = analysis_menu()
 % Display menu options at the center of the screen
 analysis_options = {'ABR', 'EFR', 'OAE', 'MEMR'};
 choice = listdlg('PromptString','Select analysis type: ','ListString',analysis_options,'SelectionMode','single','ListSize', [100 80]);
@@ -8,12 +8,17 @@ switch choice
         EXPname = 'ABR';
         EXPname2 = questdlg('Select ABR analysis:', ...
                         'ABR Analysis', ...
-                        'Thresholds','Peaks','Waveforms','Thresholds');
+                        'Thresholds','Peaks','Thresholds');
+        EXPname3 = questdlg('Select peak picking analysis:', ...
+            'ABR Peal Picking Analysis', ...
+            'Manual','DTW','Manual');
+                    
     case 2
         EXPname = 'EFR';
         EXPname2 = questdlg('Select EFR analysis:', ...
                         'EFR Analysis', ...
                         'AM/FM','RAM','RAM');
+        EXPname3 = [];
     case 3
         EXPname = 'OAE';
         OAEanalysis_options = {'DPOAE', 'SFOAE', 'TEOAE'};
@@ -28,9 +33,11 @@ switch choice
             otherwise
                 uiwait(msgbox('ERROR: Invalid selection','Analysis Type','error'));
         end
+        EXPname3 = [];
     case 4
         EXPname = 'MEMR';
         EXPname2 = [];
+        EXPname3 = [];
     otherwise
         uiwait(msgbox('ERROR: Invalid selection','Analysis Type','error'));
 end
