db3230f215caabc5058b70f72f0c3ce0685cae36
Fernando <fernandoaguileradealba@Fernandos-MacBook-Pro.local>
2025-06-17 03:08:46 -0400

    added WBMEMR threshold calculation

--- avg_memr.m
+++ avg_memr.m
@@ -12,6 +12,7 @@ avg_elicitor{1,conds} = [];
 avg_deltapow{1,conds} = [];
 all_deltapow{1,conds} = [];
 deltapow_std{1,conds} = [];
+all_threshold{1,conds} = [];
 idx = ~cellfun(@isempty,deltapow);    % find if data file is present: rows = Chins2Run, cols = Conds2Run
 if isempty(idx_plot_relative)   % plot all timepoints, including baseline
     for cols = 1:length(all_Conds2Run)
@@ -20,6 +21,9 @@ if isempty(idx_plot_relative)   % plot all timepoints, including baseline
             avg_deltapow{1,cols} = mean([avg_deltapow{1,cols}; deltapow{rows, cols}],1);
             all_deltapow{rows,cols} = deltapow{rows, cols};
             deltapow_std{1,cols} = std(cell2mat(all_deltapow(:,cols)),0,1);
+            threshold_idx = find(deltapow{rows, cols} > 0.1, 1, 'first');
+            if isempty(threshold_idx), threshold_idx = 11; end
+            all_threshold{rows,cols} = elicitor{rows, cols}(threshold_idx);
             % check if data is present for a given timepoint and subject
             if idx(rows,cols) == 1
                 % Plot individual traces with average
@@ -37,6 +41,12 @@ elseif ~isempty(idx_plot_relative)
                 avg_deltapow{1,cols-1} = mean([avg_deltapow{1,cols-1}; deltapow{rows, cols}-deltapow{rows, idx_plot_relative}],1);
                 all_deltapow{rows,cols-1} = deltapow{rows, cols}-deltapow{rows, idx_plot_relative};
                 deltapow_std{1,cols-1} = std(cell2mat(all_deltapow(:,cols-1)),0,1);
+                threshold_idx_1 = find(deltapow{rows, cols} > 0.1, 1, 'first');
+                threshold_idx_2 = find(deltapow{rows, idx_plot_relative} > 0.1, 1, 'first');
+                if isempty(threshold_idx_1), threshold_idx_1 = 11; end
+                if isempty(threshold_idx_2), threshold_idx_2 = 11; end
+                threshold = elicitor{rows, cols}(threshold_idx_1)-elicitor{rows, idx_plot_relative}(threshold_idx_2);
+                all_threshold{rows,cols-1} = threshold;
                 % check if data is present for a given timepoint and subject
                 if idx(rows,cols) == 1
                     % Plot individual traces with average
@@ -52,4 +62,5 @@ average.elicitor = avg_elicitor;
 average.deltapow = avg_deltapow;
 average.deltapow_std = deltapow_std;
 average.all_deltapow = all_deltapow;
+average.all_thresholds = all_threshold;
 end
