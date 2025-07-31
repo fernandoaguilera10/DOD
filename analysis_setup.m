0e1a394ab9cd91976d843e1a2eef55d4e12f99cc
Heinz Lab Analysis <Heinz Lab Analysis@HHSSLHS-2HZ7894.BoilerAD.Purdue.edu>
2025-07-31 12:05:37 -0400

    fixed root directory

--- analysis_setup.m
+++ analysis_setup.m
@@ -1,8 +1,7 @@
 clc; close all; clear all; warning off;
 plot_relative_flag = 1;     % Relative to Baseline:  Yes = 1   or  No = 0
 reanalyze = 0;              % 1 = redo analysis      0 = skip redo analysis
-%% ROOT and USER Directory
-ROOTdir = 'D:';
+%% USER Directory
 USERdir = 'FA';
 %% Subjects and Conditions
 chinroster_filename = 'FA_ChinRoster.xlsx';    % Animal roster Excel file
@@ -10,4 +9,4 @@ chinroster_sheet = 'DOD';   % Animal roster sheet
 Conds2Run = {strcat('pre',filesep,'Baseline'),strcat('post',filesep,'D3'),strcat('post',filesep,'D14'),strcat('post',filesep,'D28'),strcat('post',filesep,'D56')};
 Chins2Run={'Q541'};
 %% Run Analysis
-analysis_run(ROOTdir,USERdir,Chins2Run,Conds2Run,chinroster_filename,chinroster_sheet,plot_relative_flag,reanalyze);
+analysis_run('D:',USERdir,Chins2Run,Conds2Run,chinroster_filename,chinroster_sheet,plot_relative_flag,reanalyze);
