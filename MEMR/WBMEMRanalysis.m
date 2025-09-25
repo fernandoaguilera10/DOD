4b477a335291897d773d209389a2ab9e9a79a483
Fernando <fernandoaguileradealba@Fernandos-MacBook-Pro.local>
2025-09-24 22:41:43 -0400

    MEMR regression and threshold calculation

--- MEMR/WBMEMRanalysis.m
+++ MEMR/WBMEMRanalysis.m
@@ -1,11 +1,8 @@
 function WBMEMRanalysis(ROOTdir,datapath,outpath,subject,condition)% WBMEMR Analysis
 
-% DPOAE swept Analysis
 % Author: Samantha Hauser modified code from Hari Bharadwaj
 % Created: August 2023
-% Last Updated: August 23, 2023
-% Purpose:
-% Helpful info:
+% Last Updated: 23 September 2025 (FA)
 
 %% Import data
 cwd = pwd;
@@ -16,22 +13,10 @@ datafile = load_files(datapath,search_file);
 if isempty(datafile)
     return
 end
-numOfFiles = size(datafile,1);
-%% setting colors
-% Colorblind friendly continuous hue/sat changes
-cols = [103,0,31;
-    178,24,43;
-    214,96,77;
-    244,165,130;
-    253,219,199;
-    247, 247, 247;
-    209,229,240;
-    146,197,222;
-    67,147,195;
-    33,102,172;
-    5,48,97];
-cols = cols(end:-1:1, :)/255;
+%% Load MEMR template
+
 %% Analysis loop
+numOfFiles = size(datafile,1);
 for i = 1:numOfFiles
     cd(datapath)
     load(datafile);
@@ -51,8 +36,6 @@ for i = 1:numOfFiles
     semilogx(memr.freq / 1e3, memr.MEM, 'linew', 2);
     xlim([0.2, 8]);
     xticks([0.25, 0.5, 1, 2, 4, 8])
-    %axes('ColorOrder',cols);
-    %set(gca, 'XTick', ticks, 'XTickLabel', num2str(ticks'), 'FontSize', 14);
     legend(num2str(memr.elicitor'), 'FontSize', 10, 'NumColumns', 2, 'location', 'best');
     xlabel('Frequency (kHz)', 'FontSize', 14, 'FontWeight', 'bold');
     ylabel('Ear canal pressure (dB re: Baseline)', 'FontSize', 14, 'FontWeight', 'bold');
@@ -61,15 +44,37 @@ for i = 1:numOfFiles
     deltapow = power - min(power); 
     subplot(1,3,3)
     %plot(memr.elicitor, mean(abs(memr.MEM(:, memr.ind)), 2)*5, 'ok-', 'linew', 2);
-    plot(memr.elicitor, deltapow, 'ok-', 'linew', 2);
+    plot(memr.elicitor, deltapow, '*k', 'linew', 1.5,'MarkerSize',8);
     xlabel('Elicitor Level (dB FPL)', 'FontSize', 14, 'FontWeight', 'bold');
     ylabel('\Delta Absorbed Power (dB)', 'FontSize', 14, 'FontWeight', 'bold');
     ymax = max(deltapow+.05); 
     ylim([0,ymax])
     set(gca, 'XScale', 'log', 'FontSize', 14)
-    memr.threshold = interp1(deltapow, memr.elicitor, 0.1);
-    memr.deltapow = deltapow;
+
+    %% Fitting:
+    if length(deltapow) > 4      % at least 4 points needed for exponential fit
+        cor_fit = fit(memr.elicitor', deltapow,'exp1');
+        a = cor_fit.a;
+        b = cor_fit.b;
+        x_fit = memr.elicitor(1):memr.elicitor(end);
+        y_fit = a*exp(b*x_fit);
+        %Find x value on exponential that is 10% of the max
+        % x = ln(y/a)/b
+        tol = 0.10;
+        y_norm = tol*max(y_fit);
+        thresh = log(y_norm/a)/b;
+        y_thresh = a*exp(b*thresh);
+    else
+        thresh = nan;
+    end
+    hold on;
+    plot(x_fit,y_fit, 'k-', 'linew', 2);
+    plot(thresh,y_thresh,'ok','LineWidth',2,'MarkerSize',8,'MarkerFaceColor','r');
+    yline(y_thresh,'r--','LineWidth',2)
+    text(50,2*y_thresh,sprintf('Threshold:\n%.1f dB FPL',thresh));
     %% Export:
+    memr.threshold = thresh;
+    memr.deltapow = deltapow;
     cd(outpath);
     if stim.fc == 7000
         fname = [subject,'_MEMR_HP_',condition,'_',datafile(1:5)];
