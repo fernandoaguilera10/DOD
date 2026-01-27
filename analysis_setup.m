a83aae1750b80d3d755746fc15910c9a20460287
Fernando <fernandoaguileradealba@Fernandos-MacBook-Pro.local>
2026-01-27 17:31:29 -0500

    setup analysis for Ipek

--- analysis_setup.m
+++ analysis_setup.m
@@ -1,5 +1,3 @@
-%% FERNANDO WAS HERE
-
 clc; close all; clear all; warning off;
 %% ROOT Directory
 if ismac
@@ -13,7 +11,7 @@ else
 end
 %% Subjects and Conditions
 plot_relative_flag = 0;     % Relative to Baseline:  Yes = 1   or  No = 0
-reanalyze = 1;              % 1 = redo analysis      0 = skip analysis
+reanalyze = 0;              % 1 = redo analysis      0 = skip analysis
 chinroster_filename = 'DOD_ChinRoster.xlsx';    % saved under OUTdir (i.e. Analysis)
 chinroster_sheet = 'NOISE';   % 'NOISE' or 'BLAST'
 
@@ -23,7 +21,7 @@ if strcmp(chinroster_sheet,'BLAST')
     % BLAST:'Q494','Q503','Q537','Q541','Q539','Q542','Q543','Q544','Q545','Q546','Q547','Q548','Q551','Q553','Q554','Q565','Q564','Q571'
 elseif strcmp(chinroster_sheet,'NOISE')
     Conds2Run = {strcat('pre',filesep,'Baseline'),strcat('post',filesep,'D7'),strcat('post',filesep,'D14'),strcat('post',filesep,'D30')};
-    Chins2Run={'Q438','Q445','Q446','Q447','Q460','Q461','Q462','Q464','Q473','Q474','Q475','Q476','Q479','Q480','Q481','Q482','Q483','Q484','Q485','Q486','Q487','Q488','Q504','Q505'};
+    Chins2Run={'Q438'};
     % NOISE: 'Q438','Q445','Q446','Q447','Q460','Q461','Q462','Q464','Q473','Q474','Q475','Q476','Q479','Q480','Q481','Q482','Q483','Q484','Q485','Q486','Q487','Q488','Q504','Q505'
 end
 analysis_run(ROOTdir,Chins2Run,Conds2Run,chinroster_filename,chinroster_sheet,plot_relative_flag,reanalyze);
