7d057e750004c6db38a389c41f00f0fb8362e5eb
Fernando <fernandoaguileradealba@Fernandos-MacBook-Pro.local>
2026-01-27 17:38:53 -0500

    analysis setup

--- analysis_setup.m
+++ analysis_setup.m
@@ -1,27 +1,15 @@
 clc; close all; clear all; warning off;
 %% ROOT Directory
 if ismac
-    %ROOTdir = '/Volumes/heinz/data/UserTESTS/FA/DOD';  % data depot
     ROOTdir = '/Volumes/FefeSSD/DOD';
 else
-    %ROOTdir = 'Z:\data\UserTESTS\FA\DOD'; % data depot
-    %ROOTdir = 'D:\DOD'; % SSD
-    %ROOTdir = 'F:\DOD'; % NEL2
-    ROOTdir = 'E:\DOD'; % LYLE 3035 (Analysis)
+    ROOTdir = 'E:\DOD';
 end
 %% Subjects and Conditions
 plot_relative_flag = 0;     % Relative to Baseline:  Yes = 1   or  No = 0
 reanalyze = 0;              % 1 = redo analysis      0 = skip analysis
 chinroster_filename = 'DOD_ChinRoster.xlsx';    % saved under OUTdir (i.e. Analysis)
 chinroster_sheet = 'NOISE';   % 'NOISE' or 'BLAST'
-
-if strcmp(chinroster_sheet,'BLAST')
-    Conds2Run = {strcat('pre',filesep,'Baseline'),strcat('post',filesep,'D3'),strcat('post',filesep,'D14'),strcat('post',filesep,'D28')};
-    Chins2Run={'Q494','Q503','Q537','Q541','Q539','Q542','Q543','Q544','Q545','Q546','Q547','Q548','Q551','Q553','Q554','Q565','Q564','Q571'};
-    % BLAST:'Q494','Q503','Q537','Q541','Q539','Q542','Q543','Q544','Q545','Q546','Q547','Q548','Q551','Q553','Q554','Q565','Q564','Q571'
-elseif strcmp(chinroster_sheet,'NOISE')
-    Conds2Run = {strcat('pre',filesep,'Baseline'),strcat('post',filesep,'D7'),strcat('post',filesep,'D14'),strcat('post',filesep,'D30')};
-    Chins2Run={'Q438'};
-    % NOISE: 'Q438','Q445','Q446','Q447','Q460','Q461','Q462','Q464','Q473','Q474','Q475','Q476','Q479','Q480','Q481','Q482','Q483','Q484','Q485','Q486','Q487','Q488','Q504','Q505'
-end
+Conds2Run = {strcat('pre',filesep,'Baseline'),strcat('post',filesep,'D7'),strcat('post',filesep,'D14'),strcat('post',filesep,'D30')};
+Chins2Run={'Q438'};
 analysis_run(ROOTdir,Chins2Run,Conds2Run,chinroster_filename,chinroster_sheet,plot_relative_flag,reanalyze);
