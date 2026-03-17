c1f2a16111bc8a2c2af393041559e83cd160c2e2
Fernando Aguilera de Alba <66237813+fernandoaguilera10@users.noreply.github.com>
2026-03-16 20:34:55 -0400

    added instructions

--- analysis_setup.m
+++ analysis_setup.m
@@ -11,13 +11,13 @@ else
 end
 %% Subjects and Conditions
 plot_relative_flag = 0;     % Relative to Baseline:  Yes = 1   or  No = 0
-reanalyze = 0;              % 1 = redo analysis      0 = skip analysis
+reanalyze = 1;              % 1 = redo analysis      0 = skip analysis
 chinroster_filename = 'DOD_ChinRoster.xlsx';    % saved under OUTdir (i.e. Analysis)
 chinroster_sheet = 'BLAST';   % 'NOISE' or 'BLAST'
 
 if strcmp(chinroster_sheet,'BLAST')
     Conds2Run = {strcat('pre',filesep,'Baseline')};
-    Chins2Run={'Q503'};
+    Chins2Run={'Q503','Q494'};
     % BLAST:'Q494','Q503','Q537','Q541','Q539','Q542','Q543','Q544','Q545','Q546','Q547','Q548','Q551','Q553','Q554','Q565','Q564','Q571'
 elseif strcmp(chinroster_sheet,'NOISE')
     Conds2Run = {strcat('pre',filesep,'Baseline'),strcat('post',filesep,'D7'),strcat('post',filesep,'D14'),strcat('post',filesep,'D30')};
